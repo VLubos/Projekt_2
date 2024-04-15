d062b4beb8f53e09a15925209b34dbcb55573ad8
EngetoStudentVLubos <lubos.vavruska@seznam.cz>
2024-04-15 15:02:18 +0200

    Opraveny chyby

--- Projekt_2.py
+++ Projekt_2.py
@@ -17,13 +17,12 @@ Let's play a bulls and cows game.\n
    
     def duplicateCheck(checkedNumber: list) -> bool:
         randomNumber = False
-        while randomNumber is False:
-            for i in range (0, 4):
-                if checkedNumber.count(checkedNumber[i]) > 1:
-                    randomNumber is False
-                    break
-                elif checkedNumber.count(checkedNumber[i]) == 1:
-                    randomNumber is True
+        for i in range (0, 4):
+            if checkedNumber.count(checkedNumber[i]) > 1:
+                randomNumber = False
+                break
+            elif checkedNumber.count(checkedNumber[i]) == 1:
+                randomNumber = True
         return randomNumber
 
     def digitSelection() -> list:
@@ -31,7 +30,7 @@ Let's play a bulls and cows game.\n
         while numberOK is False:
             FourDigits = str(random.choice(range(1000, 9999)))
             FourDigitsToList = list(FourDigits) 
-            numberOK == duplicateCheck(FourDigitsToList)
+            numberOK = duplicateCheck(FourDigitsToList)
         return FourDigitsToList
     
     def defineNumber() -> list:
@@ -39,11 +38,11 @@ Let's play a bulls and cows game.\n
         while numberIncorrect:
             guess = input(f"Enter a number:\n{separator}\n>>> ")
             guessList = list(guess)
-            if guessList[0] == "0" or len(guessList) != 4 or duplicateCheck(guessList) is False or guess.isnumeric() is False:
+            if guessList[0] == "0" or len(guessList) != 4  or duplicateCheck(guessList) is False or guess.isnumeric() is False:
                 numberIncorrect
                 print("Incorrect entry, please try again")
             else:
-                numberIncorrect is False
+                numberIncorrect = False
         return guessList       
 
     def compareInput(guessed: list, given: list) -> list:
@@ -51,25 +50,26 @@ Let's play a bulls and cows game.\n
         indirect = 0
         for indexGuessed, memberGuessed in enumerate(guessed):
             for indexGiven, memberGiven in enumerate(given):
-                if int(memberGuessed) == memberGiven and indexGuessed == indexGiven:
+                if int(memberGuessed) == int(memberGiven) and indexGuessed == indexGiven:
                     direct += 1
-                elif int(memberGuessed) == memberGiven and indexGuessed != indexGiven:
+                elif int(memberGuessed) == int(memberGiven) and indexGuessed != indexGiven:
                     indirect += 1
         return [direct, indirect]
 
     def stringMaker(count: int, word: str) -> str:
-            if count == 1:
-                countString = (f"1 {word}")
-            if count == 0 or count == 2 or count == 3:
-                countString = (f"{count} {word}s")
-            return countString
+        if count == 1:
+            countString = (f"1 {word}")
+        elif count == 0 or count == 2 or count == 3:
+            countString = (f"{count} {word}s")
+        return countString
 
         
     comparator = digitSelection()
     tries = 0
     gameOn = True
     while gameOn:
-        result = compareInput(defineNumber(), comparator)
+        cislo = defineNumber()
+        result = compareInput(cislo, comparator)
         tries += 1
         bulls = result[0]
         cows = result[1]
